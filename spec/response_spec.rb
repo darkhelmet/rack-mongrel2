94ace2906dff2ce233e2cf1bc25605b7f1cacc80
Kevin Williams <kevwil@gmail.com>
2010-11-15 19:26:02 -0700

    flush out spec a bit more

--- spec/response_spec.rb
+++ spec/response_spec.rb
@@ -4,16 +4,27 @@ describe "Mongrel2::Response" do
   before(:each) do
     @req = double()
     @resp = double()
-    @response = Response.new(@resp)
+    @response = Mongrel2::Response.new(@resp)
   end
   
-  it "should work" do
+  it "should build the HTTP request format" do
     @req.should_receive(:uuid) {"UUID"}
     @req.should_receive(:conn_id) {"CONN_ID"}
     
-    aoeu = '%s %d:%s, %s' % [uuid, conn_id.size, conn_id, data]
-    @resp.should_receive(:send_string).with(aoeu)
+    httpreq = "UUID 7:CONN_ID, HTTP/1.1 200 OK\r\nContent-Length: 4\r\n\r\nBoo!"
+    @resp.should_receive(:send_string).with(httpreq)
     
     @response.send_http(@req, "Boo!", 200, {})
   end
+  
+  it "should send a blank response to close the response" do
+    @req.should_receive(:uuid) {"UUID"}
+    @req.should_receive(:conn_id) {"CONN_ID"}
+    
+    httpreq = "UUID 7:CONN_ID, "
+    @resp.should_receive(:send_string).with(httpreq)
+    
+    @response.close(@req)
+  end
+  
 end
