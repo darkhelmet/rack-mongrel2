bb23cc62fd3f9eee217e994a599955acdaaca52a
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2010-12-31 14:51:33 -0700

    Use correct gem name

--- lib/mongrel2.rb
+++ lib/mongrel2.rb
@@ -4,7 +4,7 @@ rescue LoadError
   begin
     require 'json'
   rescue LoadError
-    raise "You need either the yajl or json gems present in order to parse JSON!"
+    raise "You need either the yajl-ruby or json gems present in order to parse JSON!"
   end
 end
 
