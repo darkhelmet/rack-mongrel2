82794f24904b29aec2295fa04f2f4ca6b868b500
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2010-12-31 15:26:19 -0700

    Simple sinatra handler

--- example/sinatra/app.rb
+++ example/sinatra/app.rb
@@ -1,24 +1,7 @@
 require 'sinatra'
+require 'yajl/json_gem'
 
 get '*' do
-  'Hello, World!'
-  # items = request.env.map do |k,v|
-  #   "<li><strong>#{k}</strong>: #{v}</li>"
-  # end.join("\n")
-  # %Q{
-  #   <html>
-  #     <head>
-  #       <title>Sinatra Mongrel2 Test</title>
-  #     </head>
-  #     <body>
-  #       <h1>env</h1>
-  #       <ul>
-  #         #{items}
-  #       </ul>
-  #       <hr />
-  #       <h1>Params</h1>
-  #       #{params}
-  #     </body>
-  #   </html>
-  # }
+  sleep(5)
+  request.env.to_json
 end
